8b7220a7387dfc55ffd0269ec1d50ea0edd1b9fc
MingQing Zhu <leozhu333@gmail.com>
2022-09-12 16:40:22 -0700

    RestAssured test for post added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,8 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        JsonConverter.convert("jsonData2.txt");
-        List<String> li = ParentheseCombination.getCombination(3);
-        System.out.println(li);
+//        JsonConverter.convert("jsonData.txt");
+//        List<String> li = ParentheseCombination.getCombination(3);
+//        System.out.println(li);
     }
 }
