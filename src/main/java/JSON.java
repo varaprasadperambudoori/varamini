9513dc55bfb541873fea5c1f28e2139d28531d05
SkullKing96 <70766577+SkullKing96@users.noreply.github.com>
2022-09-12 14:23:12 -0700

    joshua: add java and txt file

---
+++ src/main/java/JSON.java
@@ -0,0 +1,28 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class JSON {
+
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+		JsonParser pars = new JsonParser();
+
+		try {
+			JsonObject jo = (JsonObject) pars.parse(new FileReader("/Users/Joshua/Desktop/WORK/JSON_files/test.txt"));
+			
+			FileWriter file = new FileWriter("testJSONfile.json");
+			file.write(jo.toString());
+			file.close();
+			System.out.println(jo.toString());
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
