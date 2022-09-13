9e1dd511e5eabfba60cdadc5d63f8fbb5717335e
Zeeshan Mohamedy <zee.mohamedy@gmail.com>
2022-09-13 14:52:31 -0700

    add text to JSON files and resources

---
+++ zeeshan/src/main/java/ZeeshanTxtToJson.java
@@ -0,0 +1,25 @@
+
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.*;
+import java.io.FileOutputStream;
+
+public class ZeeshanTxtToJson {
+    public static void main(String a[]) {
+        String text = "";
+        try {
+            text = new String(Files.readAllBytes(Paths.get("src/resources/jsonInput.txt")));
+            File json = new File("src/resources/output.json");
+            Writer write = new OutputStreamWriter(new FileOutputStream(json));
+            write.write(text);
+            write.flush();
+            write.close();
+            System.out.println(text);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
