a3b61aae380cb363f9e0fcabca067636b41d23f9
Simon Wu <swu21@mail.sfsu.edu>
2022-09-13 11:24:28 -0700

    Rebase + moving folders into module simon and created separate packages for txtToJson and RestAssured

---
+++ simon/RestAssuredFramework/src/main/java/RestUtil.java
@@ -0,0 +1,56 @@
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.apache.logging.log4j.LogManager;
+import io.restassured.RestAssured;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class RestUtil{
+    private final Logger LOGGER = LogManager.getLogger(RestUtil.class);
+    public ObjectMapper mapper = new ObjectMapper();
+    private String token;
+
+    public RequestSpecification given(String uri){
+        RestAssured.baseURI = uri;
+        LOGGER.info("the baseURI is "+uri);
+        RequestSpecification request = RestAssured.given();
+        return request;
+    }
+    public void addPayloadFile(RequestSpecification req, File file){
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        JsonNode json=null;
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(file))){
+            String readFile="";
+            String line = reader.readLine();
+            while(line!=null){
+                readFile+=line;
+            }
+            req.given().body(readFile);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void createProgram(String name){
+        JSONObject json = new JSONObject();
+        json.put("name",name);
+        Response res = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .post("program");
+        LOGGER.info(res.getBody().prettyPrint());
+    }
+    public void getProgramSource(String token){
+        Response res = RestAssured.given().get("program/"+token);
+        LOGGER.info(res.getBody().prettyPrint());
+
+    }
+
+}
