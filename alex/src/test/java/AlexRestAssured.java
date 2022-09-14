7c1031d875f889725dd886afb6c5abbf8728a13d
VuongAlexander <Vuongalexander99@gmail.com>
2022-09-14 15:56:12 -0700

    Added Post Method for AlexRestAssured.java

--- alex/src/test/java/AlexRestAssured.java
+++ alex/src/test/java/AlexRestAssured.java
@@ -1,7 +1,27 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.json.simple.JSONObject;
+import org.junit.Test;
 
 public class AlexRestAssured {
+    @Test
+    public void post(){
+        JSONObject param = new JSONObject();
+        param.put("name", "Program Mini");
 
-    public static void main(String[] args) {
-
+        RestAssured
+                .given()
+                    .auth()
+                    .preemptive()
+                    .basic("c598a04a-5c04-4dad-a5b5-d5dfc66e5feb", "5f791fce-154f-4d7f-ba7a-a4b44212a146")
+                    .contentType(ContentType.JSON)
+                    .body(param.toString())
+                .when().
+                    post("https://sandbox-api.marqeta.com/v3/fundingsources/program")
+                .then()
+                    .assertThat()
+                    .statusCode(201)
+                    .log()
+                .body();
     }
 }
