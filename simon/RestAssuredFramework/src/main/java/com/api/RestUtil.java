ce71ccba0d763d8a28504237d0065b65453cb061
Simon Wu <swu21@mail.sfsu.edu>
2022-09-14 20:56:13 -0700

    Restructured folders. Created Constants class for endpoints. Added create user in RestUtils. Created Test for create user. Implemented TestWatcher for assertion failures. Added new folders to separate log files

--- simon/RestAssuredFramework/src/main/java/com/api/RestUtil.java
+++ simon/RestAssuredFramework/src/main/java/com/api/RestUtil.java
@@ -1,3 +1,5 @@
+package com.api;
+
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,7 +14,7 @@ import org.json.JSONObject;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-
+import static com.api.Constants.*;
 public class RestUtil{
     private final Logger LOGGER = LogManager.getLogger(RestUtil.class);
     public ObjectMapper mapper = new ObjectMapper();
@@ -20,9 +22,9 @@ public class RestUtil{
     private RequestSpecification request;
     private String username,password;
 
-    public RestUtil given(String uri){
-        RestAssured.baseURI = uri;
-        LOGGER.info("the baseURI is "+uri);
+    public RestUtil given(){
+        RestAssured.baseURI = BASEURI;
+        LOGGER.info("the baseURI is "+RestAssured.baseURI);
         request = RestAssured.given().auth().basic(username,password);
         return this;
     }
@@ -54,14 +56,23 @@ public class RestUtil{
         JSONObject json = new JSONObject();
         json.put("name",name);
         Response res = request.contentType(ContentType.JSON)
-                .post("program");
+                .post(CREATE_PROGRAMSOURCE);
         //TODO set token
 
         LOGGER.info(res.getBody().prettyPrint());
     }
     public void getProgramSource(String token){
-        Response res = request.get("program/"+token);
+        Response res = request.get(GET_UPDATE_PROGRAMSOURCE+token);
         LOGGER.info(res.getBody().prettyPrint());
     }
-
+    public void createUser(String firstName,String lastName, String status,String email,String phoneNum){
+        JSONObject json = new JSONObject();
+        json.put("first_name",firstName);
+        json.put("last_name",lastName);
+        json.put("active",status);
+        json.put("email",email);
+        json.put("phone",phoneNum);
+        Response res = request.body(json.toString()).post(CREATE_USER);
+        LOGGER.info("Status Code: "+res.getStatusCode()+"\nPayload: "+res.getBody().prettyPrint());
+    }
 }
--- simon/RestAssuredFramework/src/test/java/SandboxAPITest/SandboxTest.java
+++ simon/RestAssuredFramework/src/test/java/SandboxAPITest/SandboxTest.java
@@ -1,9 +1,7 @@
 package SandboxAPITest;
 
 import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.config.LogConfig;
-import io.restassured.filter.log.LogDetail;
+import static com.api.Constants.*;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -13,12 +11,15 @@ import org.apache.logging.log4j.Logger;
 import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.io.PrintWriter;
 import org.apache.logging.log4j.io.IoBuilder;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
 
 public class SandboxTest {
     private static final Logger LOGGER= LogManager.getLogger(SandboxTest.class);
@@ -27,14 +28,24 @@ public class SandboxTest {
     //program mini token
     String token = "27e5e431-8459-4421-a00f-379624c9a5ad";
     //static PrintStream log;
+
+        @Rule
+        public TestWatcher watcher = new TestWatcher() {
+            @Override
+            protected void failed(Throwable e, Description description) {
+                System.out.println("I AM RIGHT HERE!");
+                LOGGER.error(""+description.getDisplayName()+" failed "+e.getMessage());
+                super.failed(e, description);
+            }
+        };
+
     @BeforeClass
     public static void setup(){
-        RestAssured.baseURI="https://sandbox-api.marqeta.com/v3/";
+        RestAssured.baseURI=BASEURI;
         //log = IoBuilder.forLogger(SandboxTest.class).setLevel(Level.ALL).buildPrintStream();
         //System.setOut(log);
         //ToLogger
         //RestAssured.config = RestAssured.config().logConfig(new LogConfig());
-
         LOGGER.info("Setup base uri");
     }
 
@@ -48,7 +59,7 @@ public class SandboxTest {
 
         Response res = req.header("Content-Type","application/json")
                 .body(json.toString())
-                .post("fundingsources/program");
+                .post(CREATE_PROGRAMSOURCE);
         Assert.assertEquals(201,res.getStatusCode());
         LOGGER.info("Pass createProgramSource\nStatus Code : "+res.getStatusCode()
                 +" Payload: "+res.getBody().prettyPrint());
@@ -62,18 +73,39 @@ public class SandboxTest {
         RequestSpecification req = RestAssured.given()
                 .auth().basic(user,password);
 
-        Response res = req.accept(ContentType.JSON).get("fundingsources/program/"+token);
+        Response res = req.accept(ContentType.JSON).get(GET_UPDATE_PROGRAMSOURCE+token);
 
         /*RequestSpecBuilder reqBuilder = new RequestSpecBuilder();
         RequestSpecification req = reqBuilder.setBaseUri("https://sandbox-api.marqeta.com/v3/").log(LogDetail.ALL).build();
 
         Response res = RestAssured.given(req).auth().basic(user,password).accept(ContentType.JSON).get("fundingsources/program/"+token);
-
          */
         Assert.assertEquals(200,res.getStatusCode());
         LOGGER.info("Pass getProgramSource\nStatus Code : "+res.getStatusCode()
                 +" Payload: "+res.getBody().prettyPrint());
+        //LOGGER.log(Level.FATAL,"Status Code: "+res.getStatusCode()+"\nPayload: "+res.getBody().prettyPrint());
     }
+    @Test
+    public void createUser(){
+        JSONObject json = new JSONObject();
+        String firstName="GoodFirstName";
+        String lastName="BadLastName";
+        String status="active";
+        String email="randomemail@fakeemail.com";
+        String phoneNum="(123)-456-6789";
 
+        json.put("first_name",firstName);
+        json.put("last_name",lastName);
+        json.put("active",status);
+        json.put("email",email);
+        json.put("phone",phoneNum);
+        RequestSpecification req = RestAssured.given()
+                .auth().basic(user,password);
+        Response res = req.body(json.toString()).post(CREATE_USER);
+        Assert.assertEquals(201,res.getStatusCode());
 
+        LOGGER.info("Create User\tStatus Code: "+res.getStatusCode()
+        +"\nPayload: "+res.getBody().prettyPrint());
+    }
 }
+
